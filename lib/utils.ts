fe3c09364f12f904e5f2b9d534302498c6d5f79c
Pablo Gonzalez <pgonzaleznetwork@gmail.com>
2023-01-25 11:11:23 +0000

    fixed type of parts() function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,6 @@
 
 const upper = (value: string): string => value.toUpperCase();
-const parts = (value: string): string => value.split('.');
+const parts = (value: string): string[] => value.split('.');
 const getObject = (value: string): string => parts(value)[0];
 const getField = (value: string): string => parts(value)[1];
 const removeWhiteSpace = (value: string): string => value.replace(/\s/g,'');
