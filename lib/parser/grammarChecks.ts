5f3b49156f38aef5cbd85792a9f66b3e3929de37
Pablo Gonzalez <pgonzaleznetwork@gmail.com>
2023-01-28 11:52:21 +0000

    replaced let with const on grammarChecks.ts

--- lib/parser/grammarChecks.ts
+++ lib/parser/grammarChecks.ts
@@ -3,55 +3,55 @@ const {upper,getField,getObject,parts} = require('../utils');
 
 type GrammarPredicate = (token:string) => boolean;
 
-let isCommentStart: GrammarPredicate = token => token == grammar.COMMENT_START;
-let isCommentEnd: GrammarPredicate = token => token ==  grammar.COMMENT_END;
+const isCommentStart: GrammarPredicate = token => token == grammar.COMMENT_START;
+const isCommentEnd: GrammarPredicate = token => token ==  grammar.COMMENT_END;
 
-let isNothing: GrammarPredicate = token => (token == null || token == undefined || token == '');
+const isNothing: GrammarPredicate = token => (token == null || token == undefined || token == '');
 
-let isNumber: GrammarPredicate = (token:string) => !isNaN(Number(token));
+const isNumber: GrammarPredicate = (token:string) => !isNaN(Number(token));
 
-let isInterestingOperator: GrammarPredicate = operator => ![',','(',')'].includes(operator);
+const isInterestingOperator: GrammarPredicate = operator => ![',','(',')'].includes(operator);
 
-let isStandardRelationship: GrammarPredicate = token => !upper(token).endsWith(grammar.RELATIONSHIP_SUFFIX);
+const isStandardRelationship: GrammarPredicate = token => !upper(token).endsWith(grammar.RELATIONSHIP_SUFFIX);
 
-let isRelationshipField: GrammarPredicate = token => token.includes('.');
+const isRelationshipField: GrammarPredicate = token => token.includes('.');
 
-let isCustom: GrammarPredicate = token => upper(token).endsWith(grammar.CUSTOM_ENTITY_SUFFIX);
+const isCustom: GrammarPredicate = token => upper(token).endsWith(grammar.CUSTOM_ENTITY_SUFFIX);
 
-let isUserField: GrammarPredicate = token => grammar.USER_FIELDS.includes(upper(getObject(token)));
+const isUserField: GrammarPredicate = token => grammar.USER_FIELDS.includes(upper(getObject(token)));
 
-let isCustomMetadata: GrammarPredicate = token => upper(token).includes(grammar.CUSTOM_METADATA_PREFIX);
+const isCustomMetadata: GrammarPredicate = token => upper(token).includes(grammar.CUSTOM_METADATA_PREFIX);
 
-let isCustomLabel: GrammarPredicate = token => upper(token).startsWith(grammar.CUSTOM_LABEL_PREFIX);
+const isCustomLabel: GrammarPredicate = token => upper(token).startsWith(grammar.CUSTOM_LABEL_PREFIX);
 
-let isCustomSetting: GrammarPredicate = token => upper(token).startsWith(grammar.CUSTOM_SETTING_PREFIX);
+const isCustomSetting: GrammarPredicate = token => upper(token).startsWith(grammar.CUSTOM_SETTING_PREFIX);
 
-let isObjectType: GrammarPredicate = token => upper(token).startsWith(grammar.OBJECT_TYPE_PREFIX);
+const isObjectType: GrammarPredicate = token => upper(token).startsWith(grammar.OBJECT_TYPE_PREFIX);
 
-let isSpecialPrefix: GrammarPredicate = token => grammar.SPECIAL_PREFIXES.includes(upper(token));
+const isSpecialPrefix: GrammarPredicate = token => grammar.SPECIAL_PREFIXES.includes(upper(token));
 
-let isParentField: GrammarPredicate = token => upper(getField(token)) == grammar.SELF_REFERENTIAL_PARENT_FIELD;
+const isParentField: GrammarPredicate = token => upper(getField(token)) == grammar.SELF_REFERENTIAL_PARENT_FIELD;
 
-let isParent: GrammarPredicate = token => upper(token) == grammar.SELF_REFERENTIAL_PARENT_OBJECT;
+const isParent: GrammarPredicate = token => upper(token) == grammar.SELF_REFERENTIAL_PARENT_OBJECT;
 
-let isProcessBuilderPrefix: GrammarPredicate = token => {
+const isProcessBuilderPrefix: GrammarPredicate = token => {
     return token.startsWith(grammar.PROCESS_BUILDER_BRACKET_START) && token.endsWith(grammar.PROCESS_BUILDER_BRACKET_END);
 }
 
-let isCPQRelationship: GrammarPredicate = token => {
+const isCPQRelationship: GrammarPredicate = token => {
 
-    let obj =  upper(getObject(token));
+    const obj =  upper(getObject(token));
 
     return obj.startsWith(grammar.CPQ_NAMESPACE) && obj.endsWith(grammar.RELATIONSHIP_SUFFIX);
 }
 
-let isOperator: GrammarPredicate = char => {
+const isOperator: GrammarPredicate = char => {
 
     if(isNothing(char)) return false;
     return grammar.OPERATORS.includes(char.trim());
 }
 
-let isFunction: GrammarPredicate = token => {
+const isFunction: GrammarPredicate = token => {
     
     if(isNothing(token)) return false;
     
