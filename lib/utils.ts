68a3deb0179fd5101d9061892825bdf1ec00099b
Pablo Gonzalez <pgonzaleznetwork@gmail.com>
2023-03-06 19:40:50 +0000

    custom string functions should upperCase input as well

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,11 +6,20 @@ const getField = (value: string): string => parts(value)[1];
 const removeWhiteSpace = (value: string): string => value.replace(/\s/g,'');
 
 String.prototype.startsWithIgnoreCase = function(input: string): boolean{
-    return this.toUpperCase().startsWith(input);
+    if(input){
+        const upperInput = input.toUpperCase();
+        return this.toUpperCase().startsWith(upperInput);
+    }
+    return false;
+    
 }
 
 String.prototype.endsWithIgnoreCase = function(input: string): boolean{
-    return this.toUpperCase().endsWith(input);
+    if(input){
+        const upperInput = input.toUpperCase();
+        return this.toUpperCase().endsWith(upperInput);
+    }
+    return false;
 }
 
 module.exports = {upper,parts,getObject,getField,removeWhiteSpace}
